2a5f80eb87e079f8dd5dbdc4f46546932fd458a5
m-d-nabeel <asharnabeel137@gmail.com>
2024-06-29 13:16:26 +0530

    fix:predict based on only last 4 words

--- app.py
+++ app.py
@@ -6,15 +6,21 @@ import joblib
 
 app = Flask(__name__)
 
-model = load_model('predict_next_words.keras')
+model = load_model("predict_next_words.keras")
 
-tokenizer = joblib.load('tokenizer')
+tokenizer = joblib.load("tokenizer")
 
 max_length = 71
 
-def predict_next_words(input_text):
+
+def predict_next_words(input_text: str):
+
+    words = input_text.split(" ")
+    if len(words) > 4:
+        input_text = " ".join(words[:-4])
+        
     token_list = tokenizer.texts_to_sequences([input_text])[0]
-    token_list = pad_sequences([token_list], maxlen=max_length+1, padding="pre")
+    token_list = pad_sequences([token_list], maxlen=max_length + 1, padding="pre")
 
     predicted_token = model.predict(token_list)
     predicted_token = np.argsort(predicted_token[0])[-3:]
@@ -23,16 +29,19 @@ def predict_next_words(input_text):
         predicted_words.append(tokenizer.index_word[token])
     return predicted_words[::-1]
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-    input_text = data['input_text']
+    input_text = data["input_text"]
     predictions = predict_next_words(input_text)
     return jsonify(predictions)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
